3ce9431b173f3ff3e2b7156f44cf634619f137c9
bravo680 <iambravo680@gmail.com>
2022-06-08 17:22:26 +0700

    create menu group component

---
+++ src/components/MenuGroup/index.jsx
@@ -0,0 +1,16 @@
+import classNames from "classnames/bind";
+
+import style from "./Menugroup.module.scss";
+
+const cx = classNames.bind(style);
+
+function MenuGroup({ title, children }) {
+  return (
+    <div className={cx("wrapper")}>
+      <div className={cx("title")}> {title}</div>
+      {children}
+    </div>
+  );
+}
+
+export default MenuGroup;
